3cd025bcff9cec393f523d99d431673bf13da31d
Sharath <sharathpavan7@gmail.com>
2019-08-09 21:47:17 +0530

    commit - Runtime permission handling added

--- app/src/main/java/com/example/odometer_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/odometer_kotlin/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.odometer_kotlin
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.NotificationCompat
+import android.support.v4.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,8 +38,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        val intent = Intent(this, OdometerService::class.java)
-        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 999)
+        } else {
+            val intent = Intent(this, OdometerService::class.java)
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        }
+
     }
 
     override fun onStop() {
@@ -46,13 +60,42 @@ class MainActivity : AppCompatActivity() {
         disPlayDistance()
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            999 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    val intent = Intent(this, OdometerService::class.java)
+                    bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+                } else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+                    val pendingIntent = PendingIntent.getActivities(this, 111, arrayOf(intent), PendingIntent.FLAG_UPDATE_CURRENT)
+
+                    val notificationBuilder = Notification.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher_round)
+                        .setContentTitle(getString(R.string.app_name))
+                        .setContentText(getString(R.string.app_name))
+                        .setAutoCancel(true)
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setVibrate(longArrayOf(0,1000))
+
+                    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    notificationManager.notify(88, notificationBuilder.build())
+
+                }
+            }
+        }
+    }
+
     private lateinit var mRunnable : Runnable
     fun disPlayDistance() {
         val handler = Handler()
         mRunnable = Runnable {
-            var distance : Double = 0.0
+            var distance : Float = 0.0f
             if (bound && odometerService != null){
-                distance = odometerService?.getDistance() ?: 0.0
+                distance = odometerService?.getDistance() ?: 0.0f
             }
             val strDistance = String.format(Locale.getDefault(), "%1$,.2f miles", distance)
             txtDistance.text = strDistance
